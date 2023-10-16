57535d73cd6aea504bdc3bc71b8928f6c942f932
onyasumi <boku@kaneki.dev>
2023-10-16 10:27:33 -0400

    implemented blink demo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::asm;
 
 mod boot {
     use core::arch::global_asm;
@@ -13,7 +14,24 @@ mod boot {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    unsafe {
+
+        // Set PIN21 to output
+        core::ptr::write_volatile(0x3F20_0008 as *mut u32, 1<<3);
+
+        loop {
+            // Turn pin on
+            core::ptr::write_volatile(0x3F20_001C as *mut u32, 1<<21);
+
+            for _ in 1..50000 {
+                asm!("nop");
+            }
+
+            // Turn pin off
+            core::ptr::write_volatile(0x3F20_0028 as *mut u32, 1<<21);
+        }
+
+    }
 }
 
 #[panic_handler]
